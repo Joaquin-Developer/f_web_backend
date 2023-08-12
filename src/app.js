3712e2910ccd2acec26ed14ec9da94d21a981d98
Joaquin-Developer <joaquin.p.olivera@gmail.com>
2023-08-12 15:33:18 -0300

    cambio puerto por default

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const app = express()
 
 // settings:
-app.set("port", process.env.PORT || 5000)
+app.set("port", process.env.PORT || 5016)
 app.use(cors({})) /* config */
 app.use(express.json())
 app.use(express.urlencoded({ extended: false })) // read html forms
